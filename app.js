fd20ef72e890523fda2ade4dfe75ab8bf3f65601
Mohd Sami <94985765+MohdSami234@users.noreply.github.com>
2022-01-01 22:43:16 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
- //Assignment -3 --
-//Q-2 Take three numbers and find max. (Using arrays) 
+ //Assignment -3 -
+// Q-1 create an array using .push with prompt and also using parseIn
  
 
 var arr = [];
@@ -17,17 +17,5 @@ i = prompt("Enter third number");
 i = parseInt(i);
 arr.push(i);
 
-   for(let i =0;i<3;i++){
-     if(arr[0]>arr[1] && arr[0] > arr[2]){
-           console.log(arr[0]);}
-       else if(arr[1]>arr[2] && arr[1] > arr[0]){
-             console.log(arr[1]);}
-           else{
-               console.log(arr[2]);}
-        }
-
-    //using Math.max()//
-    Math.max()
-    max= Math.max(arr[0],arr[1],arr[2]);
-    console.log(max);
-     
+   
+console.log(arr);
